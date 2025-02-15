dcc88c42cce06f551f75df18e0863846d6f2046a
Héléna Cardon <helenacardon62@gmail.com>
2025-02-15 03:34:32 +0000

    Vérification (pour la nième fois)

--- app.R
+++ app.R
@@ -1,20 +1,13 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
-library(glue)
 library(DT)
+library(glue)
 
 
 Diamant <- diamonds
 
-thematic_shiny(
-  font = "auto",
-)
 ui <- fluidPage(
-  theme = bs_theme(
-    version = 5,
-    bootswatch = "darkly"
-  ),
   titlePanel("Exploration des diamants"),
   sidebarLayout(
     sidebarPanel(
@@ -44,35 +37,38 @@ ui <- fluidPage(
     mainPanel(
       textOutput("DiamondTitle"),
       plotOutput("DiamondPlot"),
-      DTOutput("DiamondTable"),
+      DTOutput("DiamondTable")
     )
   )
 )
 
-
-
 server <- function(input, output) {
   rv <- reactiveValues()
+
+
   observeEvent(c(input$Prix, input$CouleurDiamant), {
-    rv$FiltreDiamant <- Diamant |>
-      filter(price < input$Prix) |>
+    rv$FiltreDiamant <- Diamant %>%
+      filter(price < input$Prix) %>%
       filter(color == input$CouleurDiamant)
   })
+
+
   output$DiamondPlot <- renderPlot({
     couleur <- ifelse(input$ChangerBleu == "yes", "blue", "grey")
-    rv$FiltreDiamant |>
-      ggplot(aes(x = carat, y = price)) +
+    ggplot(rv$FiltreDiamant, aes(x = carat, y = price)) +
       geom_point(color = couleur)
   })
-  output$DiamondTitle <- renderText({
-    rv$FiltreDiamant |>
-      nrow()
 
-    glue("Prix : {input$Prix} & Couleur du diamant : {input$CouleurDiamant}")
+
+  output$DiamondTitle <- renderText({
+    n <- nrow(rv$FiltreDiamant)
+    glue("Prix maximum : {input$Prix} & Couleur du diamant : {input$CouleurDiamant}")
   })
+
   output$DiamondTable <- renderDT({
     rv$FiltreDiamant
   })
+
   observeEvent(input$boutton, {
     showNotification(glue("Prix maximum : {input$Prix}"),
       type = "message"
@@ -83,7 +79,4 @@ server <- function(input, output) {
   })
 }
 
-
-
-shinyApp(ui = ui, server = server)
-
+shinyApp(ui, server)
