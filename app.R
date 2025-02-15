89b8d8f0dd170ffbaecbc7852d485caaf81db9b1
Héléna Cardon <helenacardon62@gmail.com>
2025-02-15 05:21:07 +0000

    N10

--- app.R
+++ app.R
@@ -5,19 +5,17 @@ library(DT)
 library(glue)
 
 
-Diamant <- diamonds
-
-
+Diamant <- ggplot2::diamonds
 
 ui <- fluidPage(
-  theme = bs_theme(version = 5, bootswatch = "minty"),
+  theme = bslib::bs_theme(version = 5, bootswatch = "minty"), 
   titlePanel("Exploration des diamants"),
   sidebarLayout(
     sidebarPanel(
       selectInput(
         inputId = "CouleurDiamant",
         label = "Choisir la couleur des diamants",
-        choices = c("D", "E", "F", "G", "H", "I", "J"),
+        choices = unique(Diamant$color),
         selected = "D"
       ),
       radioButtons(
@@ -27,10 +25,10 @@ ui <- fluidPage(
         selected = "no"
       ),
       sliderInput("Prix",
-        "Prix maximum:",
-        min = 300,
-        max = 19000,
-        value = 5000
+                  "Prix maximum:",
+                  min = min(Diamant$price),
+                  max = max(Diamant$price),
+                  value = 5000
       ),
       actionButton(
         inputId = "boutton",
@@ -46,39 +44,30 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
-  rv <- reactiveValues()
-
-
-  observeEvent(c(input$Prix, input$CouleurDiamant), {
-    rv$FiltreDiamant <- Diamant %>%
-      filter(price < input$Prix) %>%
-      filter(color == input$CouleurDiamant)
+  rv <- reactiveValues(FiltreDiamant = Diamant)
+  
+  observe({
+    rv$FiltreDiamant <- Diamant |>
+      filter(price <= input$Prix, color == input$CouleurDiamant)
   })
-
-
+  
   output$DiamondPlot <- renderPlot({
     couleur <- ifelse(input$ChangerBleu == "yes", "blue", "grey")
     ggplot(rv$FiltreDiamant, aes(x = carat, y = price)) +
       geom_point(color = couleur)
   })
-
-
+  
   output$DiamondTitle <- renderText({
-    n <- nrow(rv$FiltreDiamant)
     glue("Prix maximum : {input$Prix} & Couleur du diamant : {input$CouleurDiamant}")
   })
-
+  
   output$DiamondTable <- renderDT({
-    rv$FiltreDiamant
+    datatable(rv$FiltreDiamant, options = list(pageLength = 10))
   })
-
+  
   observeEvent(input$boutton, {
-    showNotification(glue("Prix maximum : {input$Prix}"),
-      type = "message"
-    )
-    showNotification(glue("Couleur du diamant : {input$CouleurDiamant}"),
-      type = "message"
-    )
+    showNotification(glue("Prix maximum : {input$Prix}"), type = "message")
+    showNotification(glue("Couleur du diamant : {input$CouleurDiamant}"), type = "message")
   })
 }
 
