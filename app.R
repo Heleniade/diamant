6db30e5f65fad45782f5a2f17279e9c2ea874d9e
Héléna Cardon <helenacardon62@gmail.com>
2025-02-15 00:40:08 +0000

    Version 3 (Ajout de sélection de couleur)

--- app.R
+++ app.R
@@ -9,13 +9,22 @@ Diamant <- diamonds
 
 ui <- fluidPage(
   titlePanel("Exploration des diamants"),
+  selectInput(
+    inputId = "couleur",
+    label = "Choisir la couleur",
+    choices = c(
+      "Bleu" = "blue","Rouge" = "red","Rose" = "pink","Noir" = "black",
+      "Orange" = "orange","Vert" = "green","Jaune" = "yellow","Gris"="grey"
+    ),
+    selected = "Bleu"
+  ),
   sidebarLayout(
     sidebarPanel(
       sliderInput("prix",
         "Prix:",
         min = min(Diamant$price),
         max = max(Diamant$price),
-        value = 1
+        value = min(Diamant$price)
       )
     ),
     mainPanel(
@@ -29,10 +38,11 @@ server <- function(input, output) {
   output$DiamondPlot <- renderPlot({
     Diamant |>
       filter(price > input$prix) |>
-      ggplot(aes(x = carat, y = price)) + 
-      geom_point()
+      ggplot(aes(x = carat, y = price)) +
+      geom_point(color = input$couleur) # Directly use input$couleur
   })
 }
+
 
 
 shinyApp(ui = ui, server = server)
